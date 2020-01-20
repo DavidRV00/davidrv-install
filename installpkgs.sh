a7fd8e38ffdafb38edd6a76e37f5e7ae160ecc30
David Vaughan <dvdrv00@gmail.com>
2020-01-20 11:10:20 -0800

    Add packages and install script

---
+++ installpkgs.sh
@@ -0,0 +1,27 @@
+# Apt setup
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
+sudo dpkg -i ./keyring.deb
+echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+rm keyring.deb
+sudo apt update
+
+# Packages from apt repos
+sudo apt install $(cat installs-apt)
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf &&
+$HOME/.fzf/install
+rm -rf $HOME/.fzf
+
+# miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+
+# google-chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# netrw
+echo "Visit https://www.vim.org/scripts/script.php?script_id=1075 to upgrade netrw."
